e7e57f9604258354aa9d2d4836b29ac74f4b4a3e
Wei Gao <gaow@seagroup.com>
2018-05-02 16:04:47 +0800

    Rollup config and npm package release initialization

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import sass from 'rollup-plugin-sass';
 import flow from 'rollup-plugin-flow';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import babelrc from 'babelrc-rollup';
 
 export default {
   input: 'src/index.js',
@@ -10,15 +9,14 @@ export default {
     file: 'dist/bundle.js',
     format: 'cjs',
   },
+  external: [
+    'react',
+    'prop-types',
+    'classnames'
+  ],
   plugins: [
     sass(),
-    flow(),
-    resolve({
-      // pass custom options to the resolve plugin
-      customResolveOptions: {
-        moduleDirectory: 'node_modules',
-      },
-    }),
-    babel(babelrc())
-  ],
+    resolve(),
+    babel({exclude: 'node_modules/**'})
+  ]
 };
